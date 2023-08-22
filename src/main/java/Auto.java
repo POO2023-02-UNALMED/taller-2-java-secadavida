4bac28c828f0bc08b03bd47c64a4153ab7da6b89
secadavida <142696326+secadavida@users.noreply.github.com>
2023-08-22 03:17:06 +0000

    arreglar auto

--- src/main/java/Auto.java
+++ src/main/java/Auto.java
@@ -21,7 +21,7 @@ public class Auto {
 	String verificarIntegridad() {
 		for (int i = 0; i < this.asientos.length; i++) {
 			if (this.asientos[i] == null) continue;
-			if (!(this.registro == this.motor.registro == this.asientos[i]))
+			if (!(this.registro == this.asientos[i].registro && this.registro == this.motor.registro))
 				return "Las piezas no son originales";
 		}
 		return "Auto original";
